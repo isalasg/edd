6041722550163fa1d68d0a14bc5ccb59b953678c
isala <isala@DESKTOP-06BHOCQ>
2015-08-31 19:32:21 -0500

    Creación de Nodo

---
+++ Unidad III/ListaNumeros/src/listanumeros/Nodo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listanumeros;
+
+/**
+ *
+ * @author isala
+ */
+public class Nodo {
+    private int Dato;
+    private Nodo Siguiente;
+    
+    public Nodo() {
+        this.Dato = 0;
+        this.Siguiente = null;
+    }
+
+    public int getDato() {
+        return Dato;
+    }
+
+    public void setDato(int Dato) {
+        this.Dato = Dato;
+    }
+
+    public Nodo getSiguiente() {
+        return Siguiente;
+    }
+
+    public void setSiguiente(Nodo Siguiente) {
+        this.Siguiente = Siguiente;
+    }
+    
+    
+}
