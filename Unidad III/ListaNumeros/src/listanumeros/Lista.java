a5e7ac360f89c18e8e91f3b2d92d9689e209620e
isala <isala@DESKTOP-06BHOCQ>
2015-08-31 20:58:13 -0500

    Creción de lista y de método agregar

--- Unidad III/ListaNumeros/src/listanumeros/Lista.java
+++ Unidad III/ListaNumeros/src/listanumeros/Lista.java
@@ -10,5 +10,23 @@ package listanumeros;
  * @author isala
  */
 public class Lista {
+    private Nodo Inicio;
+    private Nodo Fin;
     
+    public Lista() {
+        this.Inicio = null;
+        this.Fin = null;
+    }
+    
+    public void agregar(int numero) {
+        Nodo mNodo = new Nodo();
+        mNodo.setDato(numero);
+        
+        if (this.Inicio == null) {
+            this.Inicio = mNodo;
+        } else {
+            this.Fin.setSiguiente(mNodo);
+        }
+        this.Fin = mNodo;
+    }
 }
